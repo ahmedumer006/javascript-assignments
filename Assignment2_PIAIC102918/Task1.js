756a532d7d9277d9b3d1ec9c9a176e31344a7150
ahmedumer006 <48278965+ahmedumer006@users.noreply.github.com>
2023-03-11 17:59:08 +0500

    Added Assignment2 Task1 and Task2

---
+++ Assignment2_PIAIC102918/Task1.js
@@ -0,0 +1,5 @@
+let question = prompt("Enter Your City Here:");
+cityName = "Karachi";
+if (question==cityName) {
+    alert("Welcome To City Of Lights!");
+}
---
+++ Assignment2_PIAIC102918/Task2.js
@@ -0,0 +1,11 @@
+let question = prompt("Enter Any Traffic Signal Color:")
+color1 = "Red";
+color2 = "Yellow"
+color3 = "Green"
+if (question==color1) {
+    alert(" Must Stop!");
+} else if (question==color2) {
+    alert("Ready To Move");
+} else if (question==color3) {
+    alert("Move Now");
+}
